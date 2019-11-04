eecf38402579a27cb85ef2ff7b1a92f30705a095
AlexLiuTafe <gloria_domini@hotmail.com>
2019-11-04 15:25:51 +1100

    Initial commit

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//FOr loading scene
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+	private void Update()
+	{
+		if(Input.anyKeyDown)
+		{
+			NextScene();
+		}
+	}
+	public void NextScene()
+	{
+		//Get the current scene
+		Scene currentScene = SceneManager.GetActiveScene();
+		//Load the next scene aftercrurent scene
+		SceneManager.LoadScene(currentScene.buildIndex + 1);
+	}
+
+}
